cc7ee9416920b0b8ede4b53a8da3e760d9c3433a
davidtom <davidjtomczyk@gmail.com>
2017-07-09 15:38:54 -0400

    complete lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :title_must_be_click_bait
+
+  private
+
+  def title_must_be_click_bait
+    if !title.nil?
+      if !title.downcase.scan(/won't believe|secret|top \d+|guess/).any?
+        errors.add(:title, "Title must be clickbaity")
+      end
+    end
+  end
 end
